164baf8131baf719c078ebbed746ad78b76e9486
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-11-08 11:44:23 +0100

    Update Legion.java

--- src/ej7/Legion.java
+++ src/ej7/Legion.java
@@ -10,17 +10,20 @@ public class Legion {
 		System.out.print("Numero de legioneres: ");
 		int legioners = entrada.nextInt();
 		int shields = 0;
+		int pow = 0;
 		
 		entrada.close();
 		
 		for(int i = legioners; i > 0; i--) {
-			if(legioners / Math.pow(i, 2) >= 1) {
-				legioners -= i * i;
-				shields += Math.pow(i, 2) + 4 * i;
+			pow = i * i;
+			if(legioners / pow >= 1) {
+				legioners -= pow;
+				shields += pow + 4 * i;
 			}
 		}
 		
-		System.out.println("Shields: " + shields);
+		// Por cada legioner solo => shields += 5
+		System.out.println("Shields: " + (shields += legioners * 5));
 	}
 
 }
